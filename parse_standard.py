2026d18c11cb15136c5d996595210e22197a9107
Ken <ken.huang@mail.px.com.tw>
2025-05-29 12:14:16 +0800

    重構Basic Display Parameters，扁平化class

--- parse_standard.py
+++ parse_standard.py
@@ -10,46 +10,6 @@ class HeaderParams:
     version = "1.4"
 
 
-class BasicParams:
-    input_type = "Digital"
-    # Digital
-    color_depth = "Undefined"
-    interface = "Undefined"
-    # Analog
-    signal_level_standard = "+0.7/−0.3 V"
-    separate_sync = "Separate Sync H & V Signals are not supported"
-    composite_sync = "Composite Sync Signal on Horizontal is not supported"
-    composite_sync_green_video = "Composite Sync Signal on Green Video is not supported"
-    serrations_v_sync = "Serration on the Vertical Sync is not supported"
-
-
-class BitDepth(IntEnum):
-    UNDEFINED = 0
-    SIX_BITS = 1
-    EIGHT_BITS = 2
-    TEN_BITS = 3
-    TWELVE_BITS = 4
-    FOURTEEN_BITS = 5
-    SIXTEEN_BITS = 6
-    RESERVED = 7
-
-
-class VideoInterface(IntEnum):
-    UNDEFINED = 0
-    DVI = 1
-    HDMIa = 2
-    HDMIb = 3
-    MDDI = 4
-    DisplayPort = 5
-
-
-class SignalLevel(IntEnum):
-    ZERO = 0
-    ONE = 1
-    TWO = 2
-    THREE = 3
-
-
 class HeaderInfo:
     """解析標頭資訊"""
 
@@ -123,141 +83,207 @@ class BasicDisplayParameters:
         print()
         print(f"{'='*10}basic display parameters parse started{'='*10}")
 
-        # 判斷輸入類型，只會是Digital或Analog
-        BasicDisplayParameters._video_input_definition(edid_data)
-
-        print(f"輸入類型\t{BasicParams.input_type}")
-
-        if BasicParams.input_type == "Digital":
-            BasicDisplayParameters.ParseDigitalParams.parse_digital_params(edid_data)
-
-            print(f"色彩位元\t{BasicParams.color_depth}")
-            print(f"輸入介面\t{BasicParams.interface}")
-        else:
-            BasicDisplayParameters.ParseAnalogParams.parse_analog_params(edid_data)
-
-            print(f"訊號基準\t{BasicParams.signal_level_standard}")
-            print(f"單獨同步\t{BasicParams.separate_sync}")
-            print(f"複合同步\t{BasicParams.composite_sync}")
-            print(f"複合綠像\t{BasicParams.composite_sync_green_video}")
-            print(f"垂直同步\t{BasicParams.serrations_v_sync}")
+        print(f"顯示器輸入類型")
+        MonitorInputType.parse_manager(edid_data)
 
         print(f"{'='*10}basic display parameters parse completed{'='*10}")
+
+
+class InputTypeParams:
+    input_type = "Digital"
+    # Digital
+    color_depth = "Undefined"
+    interface = "Undefined"
+    # Analog
+    signal_level_standard = "+0.7/−0.3 V"
+    separate_sync = "Separate Sync H & V Signals are not supported"
+    composite_sync = "Composite Sync Signal on Horizontal is not supported"
+    composite_sync_green_video = "Composite Sync Signal on Green Video is not supported"
+    serrations_v_sync = "Serration on the Vertical Sync is not supported"
+
+
+class BitDepth(IntEnum):
+    UNDEFINED = 0
+    SIX_BITS = 1
+    EIGHT_BITS = 2
+    TEN_BITS = 3
+    TWELVE_BITS = 4
+    FOURTEEN_BITS = 5
+    SIXTEEN_BITS = 6
+    RESERVED = 7
+
+
+class VideoInterface(IntEnum):
+    UNDEFINED = 0
+    DVI = 1
+    HDMIa = 2
+    HDMIb = 3
+    MDDI = 4
+    DisplayPort = 5
+
+
+class SignalLevel(IntEnum):
+    ZERO = 0
+    ONE = 1
+    TWO = 2
+    THREE = 3
+
+
+class MonitorInputType:
+    """解析顯示器輸入類型"""
+
+    @staticmethod
+    def parse_manager(edid_data: bytes):
+        """組合顯示器輸入類型"""
+
+        # 判斷輸入類型，只會是Digital或Analog
+        MonitorInputType._video_input_definition(edid_data)
+        print(f"輸入類型\t{InputTypeParams.input_type}")
+
+        if InputTypeParams.input_type == "Digital":
+            ParseDigitalParams.parse_digital_params(edid_data)
+
+            print(f"色彩位元\t{InputTypeParams.color_depth}")
+            print(f"輸入介面\t{InputTypeParams.interface}")
+        else:
+            ParseAnalogParams.parse_analog_params(edid_data)
+
+            print(f"訊號基準\t{InputTypeParams.signal_level_standard}")
+            print(f"單獨同步\t{InputTypeParams.separate_sync}")
+            print(f"複合同步\t{InputTypeParams.composite_sync}")
+            print(f"複合綠像\t{InputTypeParams.composite_sync_green_video}")
+            print(f"垂直同步\t{InputTypeParams.serrations_v_sync}")
 
     @staticmethod
     def _video_input_definition(edid_data: bytes):
         """判斷輸入類型 1=Digital 0=Analog"""
         video_input_type = (edid_data[20] & 0x80) >> 7
-        BasicParams.input_type = "Digital" if video_input_type else "Analog"
+        InputTypeParams.input_type = "Digital" if video_input_type else "Analog"
 
-    class ParseDigitalParams:
+
+class ParseDigitalParams:
+    """解析數位輸入參數"""
+
+    @staticmethod
+    def parse_digital_params(edid_data: bytes):
         """解析數位輸入參數"""
 
-        @staticmethod
-        def parse_digital_params(edid_data: bytes):
-            """解析數位輸入參數"""
+        # 獲取顏色深度描述
+        ParseDigitalParams._get_bit_depth(edid_data)
+        # 獲取輸入介面描述
+        ParseDigitalParams._get_video_interface(edid_data)
 
-            # 獲取顏色深度描述
-            BasicDisplayParameters.ParseDigitalParams._get_bit_depth(edid_data)
-            # 獲取輸入介面描述
-            BasicDisplayParameters.ParseDigitalParams._get_video_interface(edid_data)
+    @staticmethod
+    def _get_bit_depth(edid_data: bytes):
+        bit_depth_value = (edid_data[20] & 0x70) >> 4
+        descriptions = {
+            BitDepth.UNDEFINED: "Undefined",
+            BitDepth.SIX_BITS: "6 bits per color",
+            BitDepth.EIGHT_BITS: "8 bits per color",
+            BitDepth.TEN_BITS: "10 bits per color",
+            BitDepth.TWELVE_BITS: "12 bits per color",
+            BitDepth.FOURTEEN_BITS: "14 bits per color",
+            BitDepth.SIXTEEN_BITS: "16 bits per color",
+            BitDepth.RESERVED: "Reserved",
+        }
+        bit_depth = BitDepth(bit_depth_value)  # Convert int to BitDepth enum
+        InputTypeParams.color_depth = descriptions.get(bit_depth, "Undefined")
 
-        @staticmethod
-        def _get_bit_depth(edid_data: bytes):
-            bit_depth_value = (edid_data[20] & 0x70) >> 4
-            descriptions = {
-                BitDepth.UNDEFINED: "Undefined",
-                BitDepth.SIX_BITS: "6 bits per color",
-                BitDepth.EIGHT_BITS: "8 bits per color",
-                BitDepth.TEN_BITS: "10 bits per color",
-                BitDepth.TWELVE_BITS: "12 bits per color",
-                BitDepth.FOURTEEN_BITS: "14 bits per color",
-                BitDepth.SIXTEEN_BITS: "16 bits per color",
-                BitDepth.RESERVED: "Reserved",
-            }
-            bit_depth = BitDepth(bit_depth_value)  # Convert int to BitDepth enum
-            BasicParams.color_depth = descriptions.get(bit_depth, "Undefined")
+    @staticmethod
+    def _get_video_interface(edid_data: bytes):
+        video_interface_value = edid_data[20] & 0x0F
+        descriptions = {
+            VideoInterface.UNDEFINED: "Undefined",
+            VideoInterface.DVI: "DVI",
+            VideoInterface.HDMIa: "HDMIa",
+            VideoInterface.HDMIb: "HDMIb",
+            VideoInterface.MDDI: "MDDI",
+            VideoInterface.DisplayPort: "DisplayPort",
+        }
+        interface = VideoInterface(
+            video_interface_value
+        )  # Convert int to VideoInterface enum
+        InputTypeParams.interface = descriptions.get(interface, "Undefined")
 
-        @staticmethod
-        def _get_video_interface(edid_data: bytes):
-            video_interface_value = edid_data[20] & 0x0F
-            descriptions = {
-                VideoInterface.UNDEFINED: "Undefined",
-                VideoInterface.DVI: "DVI",
-                VideoInterface.HDMIa: "HDMIa",
-                VideoInterface.HDMIb: "HDMIb",
-                VideoInterface.MDDI: "MDDI",
-                VideoInterface.DisplayPort: "DisplayPort",
-            }
-            interface = VideoInterface(
-                video_interface_value
-            )  # Convert int to VideoInterface enum
-            BasicParams.interface = descriptions.get(interface, "Undefined")
 
-    class ParseAnalogParams:
+class ParseAnalogParams:
+    """解析類比輸入參數"""
+
+    @staticmethod
+    def parse_analog_params(edid_data: bytes):
         """解析類比輸入參數"""
+        ParseAnalogParams._get_signal_level_standard(edid_data)
+        ParseAnalogParams._get_video_step(edid_data)
+        ParseAnalogParams._get_synchronization_types(edid_data)
+        ParseAnalogParams._get_serrations(edid_data)
 
-        @staticmethod
-        def parse_analog_params(edid_data: bytes):
-            """解析類比輸入參數"""
-            BasicDisplayParameters.ParseAnalogParams._get_signal_level_standard(
-                edid_data
-            )
-            BasicDisplayParameters.ParseAnalogParams._get_video_step(edid_data)
-            BasicDisplayParameters.ParseAnalogParams._get_synchronization_types(
-                edid_data
-            )
-            BasicDisplayParameters.ParseAnalogParams._get_serrations(edid_data)
+    @staticmethod
+    def _get_signal_level_standard(edid_data: bytes):
+        levels = (edid_data[20] & 0x60) >> 5
 
-        @staticmethod
-        def _get_signal_level_standard(edid_data: bytes):
-            levels = (edid_data[20] & 0x60) >> 5
+        descriptions = {
+            SignalLevel.ZERO: "+0.7/−0.3 V",
+            SignalLevel.ONE: "+0.714/−0.286 V",
+            SignalLevel.TWO: "+1.0/−0.4 V",
+            SignalLevel.THREE: "+0.7/0 V (EVC)",
+        }
+        levels = SignalLevel(levels)  # Convert int to WhiteSyncLevels enum
+        InputTypeParams.signal_level_standard = descriptions.get(levels, "+0.7/−0.3 V")
 
-            descriptions = {
-                SignalLevel.ZERO: "+0.7/−0.3 V",
-                SignalLevel.ONE: "+0.714/−0.286 V",
-                SignalLevel.TWO: "+1.0/−0.4 V",
-                SignalLevel.THREE: "+0.7/0 V (EVC)",
-            }
-            levels = SignalLevel(levels)  # Convert int to WhiteSyncLevels enum
-            BasicParams.signal_level_standard = descriptions.get(levels, "+0.7/−0.3 V")
+    @staticmethod
+    def _get_video_step(edid_data: bytes) -> str:
+        step = (edid_data[20] & 0x10) >> 4
+        return (
+            "Blank-to-Black setup or pedestal" if step else "Blank Level = Black Level"
+        )
 
-        @staticmethod
-        def _get_video_step(edid_data: bytes) -> str:
-            step = (edid_data[20] & 0x10) >> 4
-            return (
-                "Blank-to-Black setup or pedestal"
-                if step
-                else "Blank Level = Black Level"
-            )
+    @staticmethod
+    def _get_synchronization_types(edid_data: bytes):
+        separate_sync = (edid_data[20] & 0x08) >> 3
+        composite_sync = (edid_data[20] & 0x04) >> 2
+        composite_sync_green_video = (edid_data[20] & 0x02) >> 1
 
-        @staticmethod
-        def _get_synchronization_types(edid_data: bytes):
-            separate_sync = (edid_data[20] & 0x08) >> 3
-            composite_sync = (edid_data[20] & 0x04) >> 2
-            composite_sync_green_video = (edid_data[20] & 0x02) >> 1
+        InputTypeParams.separate_sync = (
+            "Separate Sync H & V Signals are supported"
+            if separate_sync
+            else "Separate Sync H & V Signals are not supported"
+        )
+        InputTypeParams.composite_sync = (
+            "Composite Sync Signal on Horizontal is supported"
+            if composite_sync
+            else "Composite Sync Signal on Horizontal is not supported"
+        )
+        InputTypeParams.composite_sync_green_video = (
+            "Composite Sync Signal on Green Video is supported"
+            if composite_sync_green_video
+            else "Composite Sync Signal on Green Video is not supported"
+        )
 
-            BasicParams.separate_sync = (
-                "Separate Sync H & V Signals are supported"
-                if separate_sync
-                else "Separate Sync H & V Signals are not supported"
-            )
-            BasicParams.composite_sync = (
-                "Composite Sync Signal on Horizontal is supported"
-                if composite_sync
-                else "Composite Sync Signal on Horizontal is not supported"
-            )
-            BasicParams.composite_sync_green_video = (
-                "Composite Sync Signal on Green Video is supported"
-                if composite_sync_green_video
-                else "Composite Sync Signal on Green Video is not supported"
-            )
+    @staticmethod
+    def _get_serrations(edid_data: bytes):
+        serrations = edid_data[20] & 0x01
+        InputTypeParams.serrations_v_sync = (
+            "Serration on the Vertical Sync is supported"
+            if serrations
+            else "Serration on the Vertical Sync is not supported"
+        )
 
-        @staticmethod
-        def _get_serrations(edid_data: bytes):
-            serrations = edid_data[20] & 0x01
-            BasicParams.serrations_v_sync = (
-                "Serration on the Vertical Sync is supported"
-                if serrations
-                else "Serration on the Vertical Sync is not supported"
-            )
+
+class MonitorSize:
+    """顯示器尺寸資訊"""
+
+    @staticmethod
+    def parse_manager(edid_data: bytes):
+        """組合顯示器尺寸資訊"""
+
+        horizontal = edid_data[21]  # 位址15h
+        vertical = edid_data[22]  # 位址16h
+
+        if horizontal == 0 and vertical == 0:
+            MonitorSize._parse_undefined_size()
+        elif vertical == 0:
+            MonitorSize._parse_landscape_ratio(horizontal)
+        elif horizontal == 0:
+            MonitorSize._parse_portrait_ratio(vertical)
+        else:
+            MonitorSize._parse_physical_size(horizontal, vertical)
